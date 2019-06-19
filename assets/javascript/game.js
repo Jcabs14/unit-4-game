1b5b9c7435d44fc282c0e1b781064b2c15c387cf
Jared Cabanilla <Jcabanilla@gmail.com>
2019-06-18 17:26:52 -0700

    added new images and updated css and js files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,78 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-//our variables
+    //our variables
 
-var randomResult = "";
-var wins = 0;
-var losses = 0;
-var ourTotal = 0;
+    var randomResult = "";
+    var wins = 0;
+    var losses = 0;
+    var ourTotal = 0;
 
-//set random number range
-var min = 19;
-var max = 120;
-randomResult = Math.floor(Math.random() * (max - min)) + min;
+    //set random number range
+    var min = 19;
+    var max = 120;
+    randomResult = Math.floor(Math.random() * (max - min)) + min;
 
-//random number show begining of game
-function getRandomNumber(){
-console.log(randomResult);
-document.querySelector(".total").innerHTML = randomResult;
-}
+    //random number show begining of game
+    function getRandomNumber() {
+        console.log(randomResult);
+        document.querySelector(".total").innerHTML = "Please match this number: " + randomResult;
+    }
 
-//update wins and losses
-function updateWinsAndLosses() {
-    document.querySelector(".winsAndLoss").innerHTML = "Wins: " + wins + " " + "Losses: " + losses;
-}
+    //update wins and losses
+    function updateWinsAndLosses() {
+        document.querySelector(".winsAndLoss").innerHTML = "Wins: " + wins + " " + "Losses: " + losses;
+    }
+
+    function restartGame(){
+        randomResult = Math.floor(Math.random() * (max - min)) + min;
+        getRandomNumber();
+        updateWinsAndLosses();
+    }
 
 
-//Create 4 crystals we can click on
-for (var i=0; i < 4; i++ )
-{
-    var randomNumber = Math.floor(Math.random() * 12);
-    console.log(randomNumber);
-    var crystal = $("<img>");
-    crystal.addClass("crystal");
-    crystal.attr("src", "https://picsum.photos/id/237/200/300");
-    crystal.attr("data-Value", randomNumber);
-    $(".crystalsArea").append(crystal);
-}
+    //Create 4 crystals we can click on
+    for (var i = 0; i < 4; i++) {
+        var imgArray = ["assets/images/deadpool.jpg","assets/images/magneto.jpg","assets/images/venom.jpg","assets/images/wolverine.jpg"];
+        var randomNumber = Math.floor(Math.random() * 11) + 1;
+        console.log(randomNumber);
+        var crystal = $("<img>");
+        crystal.addClass("crystal");
+        crystal.attr("src", imgArray[i]);
+        crystal.attr("data-Value", randomNumber);
+        $(".crystalsArea").append(crystal);
+    }
 
-//each crystal will have a random number we can click on to add to our total score
-$(".crystal").on("click", function(event)
-{
-    // Determining the crystal's value requires us to extract the value from the data attribute.
-    // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-    var crystalValue = ($(this).attr("data-Value"));
+    //each crystal will have a random number we can click on to add to our total score
+    $(document).on("click",".crystal", function (event) {
+        // Determining the crystal's value requires us to extract the value from the data attribute.
+        // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
+        var crystalValue = ($(this).attr("data-Value"));
 
-// Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
+        // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
 
-    crystalValue = parseInt(crystalValue);
-    ourTotal += crystalValue;
-    document.querySelector(".score").innerHTML = ourTotal;
-    console.log(ourTotal);
-});
+        crystalValue = parseInt(crystalValue);
+        ourTotal += crystalValue;
+        document.querySelector(".score").innerHTML = ourTotal;
+        console.log(ourTotal);
 
-updateWinsAndLosses();
-getRandomNumber(); 
+        if (ourTotal === randomResult) {
+            ourTotal=0;
+            alert("You win!");
+            wins++;
+            restartGame();
+        }
+
+        else if (ourTotal > randomResult) {
+            ourTotal=0;
+            alert("you lose");
+            losses++;
+            restartGame();
+        }
+
+    });
+
+    updateWinsAndLosses();
+    getRandomNumber();
 });
 
 
